192e710ce04e1a61e48e3357bfae7596845171cb
bksgiang <doandinhgiang99@gmail.com>
2021-04-01 20:41:26 +0700

    test

--- deploy.py
+++ deploy.py
@@ -14,14 +14,17 @@ def deploy():
 
 @app.route('/', methods=['POST'])
 def handle_hook():
+    print(request.json)
     if (request.json):
         req_body = request.json
         print(req_body)
         try:
-            if req_body.get('action') and req_body.get('pull_request').get('merged') and req_body.get('pull_request').get('head').get('ref') == 'production':
+            if req_body.get('action') == 'closed' and req_body.get('pull_request').get('merged') and req_body.get('pull_request').get('head').get('ref') == 'production':
+                deploy()
+            elif req_body.get('action') :
                 deploy()
         except:
             traceback.print_exc()
 
 if __name__ == "__main__":
-    app.run(port=6969)
+    app.run(port=9696)
